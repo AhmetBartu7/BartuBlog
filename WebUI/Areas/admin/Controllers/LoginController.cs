4e9dc6ad1ebe0f3dadc93f04dde2db766e8a1fc2
Ahmet Bartu <96727284+AhmetBartu7@users.noreply.github.com>
2022-04-14 13:14:09 +0300

    Add files via upload

---
+++ WebUI/Areas/admin/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Redirect("admin/Bloglar");
+        }
+    }
+}
